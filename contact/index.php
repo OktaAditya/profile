0f683f304d6ba0d316a02d1f121396532034c719
OktaAditya <75370384+OktaAditya@users.noreply.github.com>
2021-01-07 15:16:19 +0700

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet/stylesheet.css">
+</head>
+<body>
+	<header>
+        <div class="header-logo">
+            Datadiri
+        </div>
+        <div class="header-list">
+            <ul>
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About me</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <div class="main">
+        <div class="contact-form">
+            <div class="form-title">Kontak</div>
+            <form>
+                <div class="form-item">Nama</div>
+                <input class="nama" type="text" name="name">
+                <div class="form-item">Pesan</div>
+                <textarea name="body"></textarea>
+                <p></p>
+                <input class="kirim" type="submit" value="Kirim">
+            </form>
+        </div>
+    </div>
+    <footer>
+        <div class="sosial-media">
+            <div class="sosmed">
+                <a href="https://facebook.com"><img class="facebook" src="https://d2aj9sy12tbpym.cloudfront.net/assets/packs/dist/images/facebook-052c2b532c7dd80a5f78f97281b0fa20.svg"></a>
+            </div>
+            <div class="sosmed">
+                <a href="https://instagram.com"><img class="instagram" src="../sosial_media/ig.png"></a>
+            </div>
+            <div class="sosmed">
+                <a href="https://github.com"><img class="github" src="../sosial_media/gh.png"></a>
+            </div>
+            <div class="sosmed">
+                <a href="https://youtube.com"><img class="youtube" src="../sosial_media/yt.png"></a>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>© 2020 <a class="dataku" href="../index.html">Datadiri</a> | Made by Okta Ari Aditya</p>
+        </div>
+    </footer>
+</body>
+</html>
